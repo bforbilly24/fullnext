eb8aec937266a7aab41fdd077691d5d4edd08ee5
bforbilly24 <halimputra0701@gmail.com>
2024-01-22 20:43:19 +0700

    refac(host): localhost

--- libs/db.js
+++ libs/db.js
@@ -1,7 +1,7 @@
 var knex = require('knex')({
     client: 'mysql',
     connection: {
-      host: '127.0.0.1',
+      host: 'localhost',
       port: 3306,
       user: 'nextjs',
       password: 'Nextjs123##',
