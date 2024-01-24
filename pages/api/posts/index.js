8197b606e804f1d31fbe367f6de8384db88ec051
bforbilly24 <halimputra0701@gmail.com>
2024-01-25 03:25:06 +0700

    feat(authorization): jwt token header

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,6 +1,19 @@
 import db from '../../../libs/db';
 
 export default async function handler(req, res) {
+  const { authorization } = req.headers;
+  
+  if(!authorization) return res.status(401).end();
+  
+  const authSplit =  authorization.split(' ');
+  console.log(authSplit);
+  const [authType, authToken] = [
+    authSplit[0],
+    authSplit[1]
+  ]
+  
+  if(authType !== 'Bearer') return res.status(401).end();
+
   if(req.method !== 'GET') return res.status(405).end();
 
   const posts = await db('posts');
