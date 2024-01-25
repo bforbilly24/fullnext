a04bd0a4e8aeccf0d4799f400495ca8376112862
bforbilly24 <halimputra0701@gmail.com>
2024-01-25 19:30:09 +0700

    refac(auth): modify Promise

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,19 +1,24 @@
 import jwt from "jsonwebtoken";
 
 export default function hauthorization(req, res) {
-  const { authorization } = req.headers;
+  return new Promise((resolve, reject) => {
+    const { authorization } = req.headers;
 
-  if (!authorization) return res.status(401).end();
+    if (!authorization) return res.status(401).end();
 
-  const authSplit = authorization.split(" ");
-  console.log(authSplit);
-  const [authType, authToken] = [
-    authSplit[0], 
-    authSplit[1]
-];
+    const authSplit = authorization.split(" ");
+    console.log(authSplit);
+    const [authType, authToken] = [
+		authSplit[0], 
+		authSplit[1]
+	];
 
-  if (authType !== "Bearer") return res.status(401).end();
+    if (authType !== "Bearer") return res.status(401).end();
 
-    jwt.verify(authToken, "ibukuCantik");
+    return jwt.verify(authToken, "ibukuCantik", function(err, decoded) {
+		if(err) return res.status(401).end();
 
+		return resolve(decoded);
+	});
+  });
 }
