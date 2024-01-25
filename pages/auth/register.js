8ebbfdbed4707582e3d2c857040ad5dc2bfe8938
bforbilly24 <halimputra0701@gmail.com>
2024-01-25 23:28:22 +0700

    feat(hook): add react hook

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -1,17 +1,45 @@
+import React, { useState } from 'react';
+
 export default function Register() {
+	const [fields, setFields] = useState({ 
+		email: '', 
+		password: '' 
+	});
+
 	function registerHandler(e) {
 		e.preventDefault();
+
+		console.log(fields)
 	}
-	
+
+	function fieldHandler(e) {
+		const name = e.target.getAttribute('name');
+
+		setFields({
+			...fields,
+			[name]: e.target.value
+		});
+	}
+
 	return (
     <div>
       <h1>Register</h1>
 
       <form onSubmit={registerHandler.bind(this)}>
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input 
+		name='email'
+		onChange={fieldHandler.bind(this)}
+		type='text' 
+		placeholder='Email'
+		/>
+        <input
+		name='password'
+		onChange={fieldHandler.bind(this)}
+		type='password' 
+		placeholder='Password' 
+		/>
         <br />
-        <button type="submit">
+        <button type='submit'>
 			Register
 			</button>
       </form>
