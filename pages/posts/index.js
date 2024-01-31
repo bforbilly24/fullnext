c031f6afa5e93a1db1a3e44b71bffd6ee6552b20
bforbilly24 <halimputra0701@gmail.com>
2024-01-31 22:24:11 +0700

    feat(button): add edit and delete button

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -15,19 +15,37 @@ export async function getServerSideProps(ctx) {
 
 	return {
 		props: {
-            posts: posts.data
-        },
+			posts: posts.data,
+		},
 	};
 }
 
 export default function PostIndex(props) {
-	return (
+	function deleteHandler(e) {
+        e.preventDefault();
+
+        const ask = confirm('Apakah data ini akan dihapus?');
+
+        if(ask) return console.log('Delete');
+    }
+    
+    return (
 		<div>
 			<h1>Posts</h1>
 
-			{props.posts.map((post) => (
-            <div key={post.id}>{ post.title } . { post.id }</div>
-            ))}
+			{ props.posts.map((post) => (
+				<div key={post.id}>
+					<h3>{ post.title }</h3>
+                    <p>{ post.content }</p>
+
+                    <div>
+                        <button>Edit</button>
+                        <button onClick={deleteHandler.bind(this)}>Delete</button>
+                    </div>
+
+                    <hr/>
+				</div>
+			))}
 		</div>
 	);
 }
