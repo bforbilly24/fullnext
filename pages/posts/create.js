902bdfe11114a264e780f583af01bfc1fa5103f7
bforbilly24 <halimputra0701@gmail.com>
2024-01-31 12:43:53 +0700

    feat(pages): add page create post

---
+++ pages/posts/create.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { authPage } from "../../middlewares/authorizationPage";
+import Router from "next/router";
+
+export async function getServerSideProps(ctx) {
+	const { token } = await authPage(ctx);
+
+    return {
+        props: {
+            token
+        }
+    }
+}
+
+export default function PostCreate(props) {
+    const [ fields, setFields ] = useState({
+        title: '',
+        content: ''
+    });
+
+    const [ status, setStatus ] = useState('normal');
+
+    async function createHandler(e) {
+        e.preventDefault();
+
+        setStatus('loading');
+
+        const { token } = props;
+
+        const create = await fetch('/api/posts/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify(fields)
+        });
+
+        if(!create.ok) return setStatus('error');
+
+        const res = await create.json();
+
+        setStatus('success');
+
+        Router.push('/posts');
+    }
+
+    function fieldHandler(e) {
+        const name =  e.target.getAttribute('name');
+        
+        setFields({
+            ...fields,
+            [name]: e.target.value
+        });
+    }
+    
+    return(
+        <div>
+            <h1>Create a Post</h1>
+
+            <form onSubmit={createHandler.bind(this)}>
+                <input 
+                    onChange={fieldHandler.bind(this)}
+                    type="text" 
+                    placeholder="Title" 
+                    name="title" 
+                />
+                <br />
+                <textarea 
+                    onChange={fieldHandler.bind(this)}
+                    type="text" 
+                    placeholder="Content" 
+                    name="content" 
+                ></textarea>
+                <br />
+
+                <button type="submit">
+                    Create Post
+                </button>
+
+                <div>
+                    status: {status}
+                </div>
+            </form>
+        </div>
+    )
+}
