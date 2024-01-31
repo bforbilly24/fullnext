917e89576359661d49c14ade678270a5675ad1ea
bforbilly24 <halimputra0701@gmail.com>
2024-02-01 04:25:51 +0700

    refac(env): change jwt secret to load environment variables

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -7,12 +7,12 @@ export default function hauthorization(req, res) {
 		if (!authorization) return res.status(401).end();
 
 		const authSplit = authorization.split(' ');
-		console.log(authSplit);
-		const [authType, authToken] = [authSplit[0], authSplit[1]];
+
+        const [authType, authToken] = [authSplit[0], authSplit[1]];
 
 		if (authType !== 'Bearer') return res.status(401).end();
 
-		return jwt.verify(authToken, 'ibukuCantik', function (err, decoded) {
+		return jwt.verify(authToken, process.env.JWT_SECRET, function (err, decoded) {
 			if (err) return res.status(401).end();
 
 			return resolve(decoded);
